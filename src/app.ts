c365915fa79e9c53811dba32af9d4005aa1168be
meng <monkeystylle@gmail.com>
2026-08-27 10:44:52 +0800

    add trust proxy

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { authenticate } from './middleware/authenticate.js';
 
 const app = express();
 
+app.set('trust proxy', 2);
+
 app.use(express.json());
 
 app.get('/healthcheck', (_req, res) => {
